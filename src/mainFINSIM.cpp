a81d1b7e1e342a1bbef3177101fff89da48d7ce5
jor-ch <chtoh97@gmail.com>
2026-03-15 23:29:38 +0800

    Added code in mainFINSIM to test the newly-added functions for Stock. Note that updateStockMultiplier() is still not done, it scales the stock price by 1.1 times by default.
    Added code in mainFINSIM to test the newly-added functions for Stock. Note that updateStockMultiplier() is still not done, it scales the stock price by 1.1 times by default.

--- src/mainFINSIM.cpp
+++ src/mainFINSIM.cpp
@@ -7,6 +7,8 @@ int main()
     Cash client_cash(2000.0);
     Stock snp500(350.0);
     double stock_expense = snp500.purchaseStock(3.0);
+    snp500.updateStockMultiplier();
+    snp500.updateValue(0.0);
     client_cash.updateValue(-stock_expense);
     std::cout << "client stock quantity and value is " << snp500.quantity
               << " , " << snp500.value << std::endl;
