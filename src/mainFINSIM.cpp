e813dbd13ffa75be3ab1eca530bd2d94550c379b
jor-ch <chtoh97@gmail.com>
2026-03-15 23:20:24 +0800

    Test code to ensure Cash and Stock classes are functioning as intended
    Test code to ensure Cash and Stock classes are functioning as intended

--- src/mainFINSIM.cpp
+++ src/mainFINSIM.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "Cash.h"
+#include "Stock.h"
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    Cash client_cash(2000.0);
+    Stock snp500(350.0);
+    double stock_expense = snp500.purchaseStock(3.0);
+    client_cash.updateValue(-stock_expense);
+    std::cout << "client stock quantity and value is " << snp500.quantity
+              << " , " << snp500.value << std::endl;
+    std::cout << "client is left with " << client_cash.value << std::endl;
     return 0;
 }
