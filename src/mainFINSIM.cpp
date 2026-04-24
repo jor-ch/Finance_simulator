1ebb3e5405836384fe176ac0ed4d66da5f172c40
jor-ch <chtoh97@gmail.com>
2026-04-24 17:14:12 +0800

    Changed code in main to test the implementation of the overloaded constructor for Stock class.
    Changed code in main to test the implementation of the overloaded constructor for Stock class.

--- src/mainFINSIM.cpp
+++ src/mainFINSIM.cpp
@@ -4,14 +4,15 @@
 
 int main()
 {
-    Cash client_cash(2000.0);
-    Stock snp500(350.0);
-    double stock_expense = snp500.purchaseStock(3.0);
-    snp500.updateStockMultiplier();
-    snp500.updateValue(0.0);
-    client_cash.updateValue(-stock_expense);
-    std::cout << "client stock quantity and value is " << snp500.quantity
-              << " , " << snp500.value << std::endl;
-    std::cout << "client is left with " << client_cash.value << std::endl;
+    Stock snp500("../data/SNP500_monthly_price_1871_to_Mar_2026.csv"); // ../ since directory is in build folder, so need to go back one folder
+    // Cash client_cash(2000.0);
+    // Stock snp500(350.0);
+    // double stock_expense = snp500.purchaseStock(3.0);
+    // snp500.updateStockMultiplier();
+    // snp500.updateValue(0.0);
+    // client_cash.updateValue(-stock_expense);
+    // std::cout << "client stock quantity and value is " << snp500.quantity
+    //           << " , " << snp500.value << std::endl;
+    // std::cout << "client is left with " << client_cash.value << std::endl;
     return 0;
 }
