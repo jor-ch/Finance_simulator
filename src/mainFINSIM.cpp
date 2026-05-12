c940293fe00f60fe8a72931807f40047683f184b
jor-ch <chtoh97@gmail.com>
2026-05-12 11:42:11 +0800

    changed test code in main to test interaction with Cash and Stock classes
    changed test code in main to test interaction with Cash and Stock classes

--- src/mainFINSIM.cpp
+++ src/mainFINSIM.cpp
@@ -7,21 +7,17 @@ int main()
 {
     Stock snp500("../data/SNP500_monthly_price_1871_to_Mar_2026.csv", 1973, 11); // ../ since directory is in build folder, so need to go back one folder
     Cash bankAccount(0.0);                                                       // start with $0
+    const double salary = 200.0;
     for (int i = 0; i < 10; i++)
     {
+        bankAccount.updateValue(salary); // simulate person getting salary
         snp500.updateParameters();
-        std::cout << "price per stock now is " << snp500.GetPricePerStock() << std::endl;
-        std::cout << "current stock value before purchase is " << snp500.GetValue() << std::endl;
-        if (i % 2 == 0)
-        {
-            snp500.purchaseStock(1.0);
-        }
-        else
-        {
-            snp500.purchaseStockByPrice(200);
-        }
-
-        std::cout << "stock quantity is " << snp500.GetStockQuantity() << " and stock value is " << snp500.GetValue() << std::endl;
+        double currentMoneyAvailable = bankAccount.GetValue();
+        bankAccount.updateValue(-1.0 * snp500.purchaseStockByPrice(currentMoneyAvailable));
+        std::cout << "bank account has " << bankAccount.GetValue() << " and stock value is "
+                  << snp500.GetValue() << " and stock quantity is "
+                  << snp500.GetStockQuantity() << " and price per stock is "
+                  << snp500.GetPricePerStock() << std::endl;
     }
     return 0;
 }
